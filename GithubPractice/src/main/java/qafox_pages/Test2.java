2f197561fbeb249cb881af5be2ac3b0c62d477fa
Duherq <138963733+Duherq@users.noreply.github.com>
2023-07-08 19:01:04 +0530

    Update

--- GithubPractice/src/main/java/qafox_pages/Test2.java
+++ GithubPractice/src/main/java/qafox_pages/Test2.java
@@ -4,13 +4,7 @@ public class Test2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Push");
 		
-		System.out.println("Pull");
-
-		System.out.println("Change");
-		
-		System.out.println("Change2");
 		
 
 	}
