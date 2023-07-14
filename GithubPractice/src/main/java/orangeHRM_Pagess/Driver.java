47a07618ab024aa867e235eec2d18696706754a3
Duherq <138963733+Duherq@users.noreply.github.com>
2023-07-14 11:40:57 +0530

    Update

--- GithubPractice/src/main/java/orangeHRM_Pagess/Driver.java
+++ GithubPractice/src/main/java/orangeHRM_Pagess/Driver.java
@@ -18,8 +18,7 @@ public class Driver
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		String actual_url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 	    String expected_url=driver.getCurrentUrl();
-	    Assert.assertEquals(actual_url, expected_url,"Sorry your not navigate to OrangeHRM Login page");
-	    
+	    Assert.assertEquals(actual_url, expected_url,"Sorry your not navigate to OrangeHRM Login page");    
 	
 	}
 	 public static void CloseBrowser()
