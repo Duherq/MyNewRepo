d9b2d958155f1d183f47bed8c704eaeefa42fb3d
Duherq <138963733+Duherq@users.noreply.github.com>
2023-07-26 18:43:30 +0530

    Cucumber QAFOX_AccountRegistrationTestCase

--- Cucumber_Project/src/test/java/qafox_TestCase/Account_RegistrationTestCase.java
+++ Cucumber_Project/src/test/java/qafox_TestCase/Account_RegistrationTestCase.java
@@ -1,8 +1,11 @@
 package qafox_TestCase;
 
+import org.testng.Assert;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import qafox_pages.QAFOX_Common_Functions;
+import qafox_pages.QAFOX_RegisterAccount_Page;
 
 public class Account_RegistrationTestCase extends QAFOX_Common_Functions
 {
@@ -11,108 +14,155 @@ public class Account_RegistrationTestCase extends QAFOX_Common_Functions
 	{
 	    // Write code here that turns the phrase above into concrete actions
 	    open_browser(string);
+	    String actual_url="https://tutorialsninja.com/demo/index.php?route=account/register";
+		String expected_url=driver.getCurrentUrl();
+		Assert.assertEquals(actual_url, expected_url,"Successfully Navigate to QAFOX Create Account page");	
 	}
+	
 
 	@Then("QAFOX Account registration page should be displayed")
-	public void qafox_account_registration_page_should_be_displayed() {
+	public void qafox_account_registration_page_should_be_displayed() 
+	{
+	    // Write code here that turns the phrase above into concrete actions
+		
+	}
+
+	@Given("firstname:{string}")
+	public void firstname(String string) 
+	{
+		String actual_firstname="Duher";
+	    // Write code here that turns the phrase above into concrete actions
+		QAFOX_RegisterAccount_Page.click_firstname_inputbox();
+		QAFOX_RegisterAccount_Page.enter_firstname(string);
+	    Assert.assertEquals(actual_firstname,QAFOX_RegisterAccount_Page.firstname_inputbox().getAttribute("value"),"Expected firstname should be entered successfully");
+
+	}
+
+	@Then("Firstname entered successfully")
+	public void firstname_entered_successfully() 
+	{
+		
+	}
+
+	@Given("lastname:{string}")
+	public void lastname(String string) 
+	{
+		String actual_lastname="Quazi";
+	    // Write code here that turns the phrase above into concrete actions
+	    QAFOX_RegisterAccount_Page.click_lastname_inputbox();
+	    QAFOX_RegisterAccount_Page.enter_lastname(string);
+	    Assert.assertEquals(actual_lastname, QAFOX_RegisterAccount_Page.lastname_inputbox().getAttribute("value"),"Expected lastname should not be entered ");
+	    
+	}
+
+	@Then("Lastname entered successfully")
+	public void lastname_entered_successfully() 
+	{
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Given("Email:{string}")
+	public void email(String string) 
+	{
+		String actual_email="duher@gmail.com";
+	    // Write code here that turns the phrase above into concrete actions
+	    QAFOX_RegisterAccount_Page.click_email_inputbox();
+	    QAFOX_RegisterAccount_Page.enter_email(string);
+	    Assert.assertEquals(actual_email, QAFOX_RegisterAccount_Page.email_inputbox().getAttribute("value"),"Expected lastname should not be entered successfully");
+	   
+	}
+
+	@Then("Email entered successfully")
+	public void email_entered_successfully() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
 	    
 	}
 
-	@Given("firstname:{string}")
-	public void firstname(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("Firstname entered successfully")
-	public void firstname_entered_successfully() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("lastname:{string}")
-	public void lastname(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("Lastname entered successfully")
-	public void lastname_entered_successfully() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("Email:{string}")
-	public void email(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("Email entered successfully")
-	public void email_entered_successfully() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
 	@Given("Telephon number:{string}")
-	public void telephon_number(String string) {
+	public void telephon_number(String string) 
+	{
+		String actual_phoneNo="9999999999";
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    QAFOX_RegisterAccount_Page.click_telephon_inputbox();
+		QAFOX_RegisterAccount_Page.enter_telephonNo(string);
+	    Assert.assertEquals(actual_phoneNo, QAFOX_RegisterAccount_Page.telephon_inputbox().getAttribute("value"),"Expected phon number should not be entered ");
+	
 	}
 
 	@Then("Telephon number entered successfully")
-	public void telephon_number_entered_successfully() {
+	public void telephon_number_entered_successfully() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Given("Password:{string}")
-	public void password(String string) {
+	public void password(String string) 
+	{
+		String actual_password="Duher123";
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    QAFOX_RegisterAccount_Page.click_password_inputbox();
+	    QAFOX_RegisterAccount_Page.enter_password(string);
+	    Assert.assertEquals(actual_password,QAFOX_RegisterAccount_Page.password_inputbox().getAttribute("value"),"Expected password should not be entered ");
+
 	}
 
 	@Then("Password entered successfully")
-	public void password_entered_successfully() {
+	public void password_entered_successfully()
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	
 	}
 
 	@Given("Confirm password:{string}")
-	public void confirm_password(String string) {
+	public void confirm_password(String string) 
+	{
+		String actual_password="Duher123";
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    QAFOX_RegisterAccount_Page.click_confirmPassword_inputbox();
+	    QAFOX_RegisterAccount_Page.enter_confirmPassword(string);
+	    Assert.assertEquals(actual_password,QAFOX_RegisterAccount_Page.confirmPassword_inputbox().getAttribute("value"),"Expected confirm password should not be entered");
+	    
 	}
 
 	@Then("Confirm Password entered successfully")
-	public void confirm_password_entered_successfully() {
+	public void confirm_password_entered_successfully() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("Check subscribe radio button yes option is not checked")
-	public void check_subscribe_radio_button_yes_option_is_not_checked() {
+	public void check_subscribe_radio_button_yes_option_is_not_checked() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   Assert.assertFalse(QAFOX_RegisterAccount_Page.subscribeoption_yes().isSelected(),"Yes Option is selected it is not expected");
+
 	}
 
 	@Then("Expected radio button should be checked")
-	public void expected_radio_button_should_be_checked() {
+	public void expected_radio_button_should_be_checked() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 	@Then("Click privacy policy checkbox")
-	public void click_privacy_policy_checkbox() {
+	public void click_privacy_policy_checkbox() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		QAFOX_RegisterAccount_Page.click_privacy_policy_checkbox();
+	    Assert.assertTrue(QAFOX_RegisterAccount_Page.privacy_policy_checkbox().isSelected(), "Privacy checkbox is not clicked");
+	
 	}
 
 	@Then("Privacy policy checkbox should be displayed Checked")
-	public void privacy_policy_checkbox_should_be_displayed_checked() {
+	public void privacy_policy_checkbox_should_be_displayed_checked() 
+	{
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 
 
