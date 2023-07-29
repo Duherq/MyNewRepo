4d4371c92f6fa5cf6e15508f243e358843a3c39b
Duherq <138963733+Duherq@users.noreply.github.com>
2023-07-29 11:57:03 +0530

    Test_Case Update

--- PlayWright_Project/src/main/java/qafox_TestCases/QAFOX_RegisterAccount_TestCase.java
+++ PlayWright_Project/src/main/java/qafox_TestCases/QAFOX_RegisterAccount_TestCase.java
@@ -42,33 +42,76 @@ public class QAFOX_RegisterAccount_TestCase extends QAFOX_BaseFunctionality
 		check_navigate_QAFOXRegisterAccountPage();
 		QAF_RegisterAccount_Page.click_firstname_inputbox();
 		QAF_RegisterAccount_Page.enter_firstname("Duher");
-	    Assert.assertEquals(actual_firstname,QAF_RegisterAccount_Page.firstname_inputbox().textContent(),"Expected firstname should be entered successfully");
+	    Assert.assertEquals(actual_firstname,QAF_RegisterAccount_Page.firstname_inputbox().inputValue(),"Expected firstname should be entered successfully");
 	    
 	    
 	    QAF_RegisterAccount_Page.click_lastname_inputbox();
 	    QAF_RegisterAccount_Page.enter_lastname("Quazi");
-	    Assert.assertEquals(actual_lastname, QAF_RegisterAccount_Page.lastname_inputbox().getAttribute("value"),"Expected lastname should not be entered ");
+	    Assert.assertEquals(actual_lastname, QAF_RegisterAccount_Page.lastname_inputbox().inputValue(),"Expected lastname should not be entered ");
 	    
 	    QAF_RegisterAccount_Page.click_email_inputbox();
 	    QAF_RegisterAccount_Page.enter_email("duher123@gmail.com");
-	    Assert.assertEquals(actual_email, QAF_RegisterAccount_Page.email_inputbox().getAttribute("value"),"Expected lastname should not be entered successfully");
+	    Assert.assertEquals(actual_email, QAF_RegisterAccount_Page.email_inputbox().inputValue(),"Expected lastname should not be entered successfully");
 	    
 	    QAF_RegisterAccount_Page.click_telephon_inputbox();
 	    QAF_RegisterAccount_Page.enter_telephonNo("9999999999");
-	    Assert.assertEquals(actual_phoneNo, QAF_RegisterAccount_Page.telephon_inputbox().getAttribute("value"),"Expected phon number should not be entered ");
+	    Assert.assertEquals(actual_phoneNo, QAF_RegisterAccount_Page.telephon_inputbox().inputValue(),"Expected phon number should not be entered ");
 	
 	    QAF_RegisterAccount_Page.click_password_inputbox();
 	    QAF_RegisterAccount_Page.enter_password("Duher@123");
-	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.password_inputbox().getAttribute("value"),"Expected password should not be entered ");
+	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.password_inputbox().inputValue(),"Expected password should not be entered ");
 
 	    QAF_RegisterAccount_Page.click_confirmPassword_inputbox();
 	    QAF_RegisterAccount_Page.enter_confirmPassword("Duher@123");
-	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.confirmPassword_inputbox().getAttribute("value"),"Expected confirm password should not be entered");
+	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.confirmPassword_inputbox().inputValue(),"Expected confirm password should not be entered");
 	    
 	    Assert.assertFalse(QAF_RegisterAccount_Page.subscribeoption_yes().isChecked(),"Yes Option is selected it is not expected");
 	    
 	    QAF_RegisterAccount_Page.click_privacy_policy_checkbox();
 	    Assert.assertTrue(QAF_RegisterAccount_Page.privacy_policy_checkbox().isChecked(), "Privacy checkbox is not clicked");
+	}
+	
+	@Test(priority=3,description="TestCase:Validate Register Account with blank one mandatory field",groups="Register account")
+	public void validate_registerAccount_blankOne_mandatoryField() throws Exception
+	{
+		String actual_firstname="Duher";
+		String actual_email="duher123@gmail.com";
+		String actual_phoneNo="9999999999";
+		String actual_password="Duher@123";
+
+		
+		check_navigate_QAFOXRegisterAccountPage();
+		QAF_RegisterAccount_Page.click_firstname_inputbox();
+		QAF_RegisterAccount_Page.enter_firstname("Duher");
+	    Assert.assertEquals(actual_firstname,QAF_RegisterAccount_Page.firstname_inputbox().inputValue(),"Expected firstname should be entered successfully");
+	   
+	    String empty=QAF_RegisterAccount_Page.lastname_inputbox().inputValue();
+	    Assert.assertTrue(empty.isEmpty(), actual_password);
+	    
+	    QAF_RegisterAccount_Page.click_email_inputbox();
+	    QAF_RegisterAccount_Page.enter_email("duher123@gmail.com");
+	    Assert.assertEquals(actual_email, QAF_RegisterAccount_Page.email_inputbox().inputValue(),"Expected lastname should not be entered successfully");
+	    
+	    QAF_RegisterAccount_Page.click_telephon_inputbox();
+	    QAF_RegisterAccount_Page.enter_telephonNo("9999999999");
+	    Assert.assertEquals(actual_phoneNo, QAF_RegisterAccount_Page.telephon_inputbox().inputValue(),"Expected phon number should not be entered ");
+	
+	    QAF_RegisterAccount_Page.click_password_inputbox();
+	    QAF_RegisterAccount_Page.enter_password("Duher@123");
+	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.password_inputbox().inputValue(),"Expected password should not be entered ");
+
+	    QAF_RegisterAccount_Page.click_confirmPassword_inputbox();
+	    QAF_RegisterAccount_Page.enter_confirmPassword("Duher@123");
+	    Assert.assertEquals(actual_password,QAF_RegisterAccount_Page.confirmPassword_inputbox().inputValue(),"Expected confirm password should not be entered");
+	    
+	    Assert.assertFalse(QAF_RegisterAccount_Page.subscribeoption_yes().isChecked(),"Yes Option is selected it is not expected");
+	    
+	    QAF_RegisterAccount_Page.click_privacy_policy_checkbox();
+	    Assert.assertTrue(QAF_RegisterAccount_Page.privacy_policy_checkbox().isChecked(), "Privacy checkbox is not clicked");
+	    
+	    QAF_RegisterAccount_Page.click_continue_button();
+	    Assert.assertTrue(QAF_RegisterAccount_Page.blankfield_errormsg().isVisible(),"Expected Error message should not be displayed");
+		
 	}
 
 }
